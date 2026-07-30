034cb558c90024ab87d86c7601e88b18d22f94cc
OguzHanYav <hanouz34@hotmail.com>
2026-07-30 16:52:51 +0300

    Fix: Add card assets to dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,17 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'assets/[name].[hash].js',
+          entryFileNames: 'assets/[name].[hash].js',
+        },
+      },
     },
     publicDir: 'public',
+    server: {
+      port: 3000,
+    },
   };
 });
